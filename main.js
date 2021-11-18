0877708d0ea590da98f416ae119b0877754ccc4b
boukerdennaa <boukerdennaa@gmail.com>
2021-11-18 08:32:06 +0100

    cambio

--- main.js
+++ main.js
@@ -3,20 +3,3 @@ WebFont.load({
         families: ['Ubuntu:700,700i', 'Ubuntu+Condensed:400']
     }
 });  
-
-var slideIndex= 0;
-function showSlides(){
-    var slides = document.getElementsByClassName("mySlides");
-    console.log("funciona");
-    for( i=0; i < slides.length; i++){
-        slides[i].style.display="none";
-    }
-
-    slideIndex++;
-    if (slideIndex > slides.length){
-        slideIndex=1;
-    }
-
-    slides[slideIndex-1].style.display= "block";
-    setTimeout(showSlides, 2000);
-}    
